1221e45a1eb6cc0a2af05a4ba978c28534b45819
Michael Meade <crazyace003@gmail.com>
2018-08-18 23:48:41 -0500

    Add files via upload

---
+++ lib/api.rb
@@ -0,0 +1,81 @@
+require 'mechanize'
+module Bleeping
+  class << self
+    public
+    #/html/body/div[1]/section[3]/div/div/div[1]/div/div[1]/div[1]/div[2]/ul/li[1]
+    Address = "https://bleepingcomputer.com"
+    Agent   = Mechanize.new
+    Page    = Agent.get(Address)
+    def Option(num)
+        if num.nil? or num < 1
+            link  = Page.at("#bc-home-news-main-wrap > li:nth-child(1) > div.bc_latest_news_text > h4 > a")
+            return link
+        else
+            link  = Page.at("#bc-home-news-main-wrap > li:nth-child("+num.to_s+") > div.bc_latest_news_text > h4 > a")
+            return link
+        end
+    end
+    def get(*args)
+        # Get title/link at any place
+    	num     = args.shift
+        results = Option(num)
+        return results[:href], results.text
+    end
+    def Title(*args)
+        # Just get title
+        num     = args.shift
+        results = Option(num)
+        return results.text
+    end
+    def Link(*args)
+        # Just get link
+        num     = args.shift
+        results = Option(num)
+        return results[:href]
+    end
+    def Date(*args)
+        num  = args.shift
+        if num.nil? or num < 1
+            link  = Page.at("#bc-home-news-main-wrap > li:nth-child(1) > div.bc_latest_news_text > ul > li.bc_news_date")
+            return link
+        else
+            link  = Page.at("#bc-home-news-main-wrap > li:nth-child("+num.to_s+") > div.bc_latest_news_text > ul > li.bc_news_date")
+            return link.text
+        end
+    end
+    def Time(*args)
+        num = args.shift
+        if num.nil? or num < 1
+            link = Page.at("#bc-home-news-main-wrap > li:nth-child(1) > div.bc_latest_news_text > ul > li.bc_news_time")
+            return link.text
+        else
+            link = Page.at("#bc-home-news-main-wrap > li:nth-child("+num.to_s+") > div.bc_latest_news_text > ul > li.bc_news_time")
+            return link.text
+        end
+    end
+    def Author(*args)
+        num = args.shift
+        if num.nil? or num < 1
+            link = Page.at("#bc-home-news-main-wrap > li:nth-child(1) > div.bc_latest_news_text > ul > li.bc_news_author > a")
+        else
+            link = Page.at("#bc-home-news-main-wrap > li:nth-child("+num.to_s+") > div.bc_latest_news_text > ul > li.bc_news_author > a")
+            return link.text
+        end
+    end
+    def Summary(*args)
+        num = args.shift
+        if num.nil? or num < 1
+            summary = Page.at("#bc-home-news-main-wrap > li:nth-child(1) > div.bc_latest_news_text > p")
+            return summary.text
+        else
+            summary = Page.at("#bc-home-news-main-wrap > li:nth-child("+num.to_s+") > div.bc_latest_news_text > p")
+            return summary.text
+        end
+    end
+    def BleepingComputer(*args)
+        num = args.shift
+        a = Hash["Title" => get(num)[1], "Link" => get(num)[0], "Date" => Date(num), "Time" => Time(num), "Author" => Author(num), "Summary" => Summary(num)]
+        return a
+    end
+  end
+end
